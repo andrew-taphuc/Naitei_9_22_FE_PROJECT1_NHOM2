04e15e5d635185a792b985aee7a8f9b83c2222cc
Ta Hong Phuc <taphuc1@gmail.com>
2025-08-25 15:13:08 +0700

    eslint

--- project1/eslint.config.mjs
+++ project1/eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off", 
+      "react-hooks/exhaustive-deps": "off",
+      "react/no-children-prop": "off",
+      "@typescript-eslint/no-empty-object-type": ["error", { allowInterfaces: "always" }],
+      "prefer-const": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
